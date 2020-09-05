7fb412673a33afa9cb06942df09a1658390d96a7
rjhall90 <rjhall@gmx.us>
2020-09-05 02:14:36 -0400

    Added body parser, added PGSQL connections for SQL server functions, route comments for posterity

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const bodyParser = require('body-parser');
 const requiredRoutes = require('./routes/required');
 
 const app = express();
 
 //app.use(helmet());
 
+app.use(bodyParser.json());
+
 app.use('/api', requiredRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send({ 'error': 'Route not found' });
 });
-console.log("Listening on Port: ${process.env.PORT}");
+console.log(`Listening on Port: ${process.env.PORT}`);
 app.listen(process.env.PORT);
--- routes/required.js
+++ routes/required.js
@@ -1,26 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+client.connect();
 
+// /api/list
+// Get all records
 router.get('/list', (req, res, next) => {
-    res.send({ records: [] });
+    client
+        .query(`SELECT * FROM get_records();`)
+        .then(resp => res.send({ message: `Found ${resp.rows.length} rows`, data: resp.rows }))
+        .catch(err => res.status(500).send(err));
 });
 
+// /api/create
+// Create a new record
 router.post('/create', (req, res, next) => {
-    res.status(403).send();
+    if (!req.body.value1 || !req.body.value2 || !req.body.value3) return res.status(400).send({ message: 'All three values must be provided to complete this request' });
+    client
+        .query(`SELECT * FROM insert_record('${req.body.value1}',${req.body.value2},${req.body.value3});`)
+        .then(resp => res.send({ message: `New row created`, data: resp.rows }))
+        .catch(err => res.status(500).send(err));
 });
 
+// /api/red/:recordId
+// Get a specific record by id
 router.get('/read/:recordId', (req, res, next) => {
-    res.send(req.params);
+    client
+        .query(`SELECT * FROM get_record(${req.params.recordId});`)
+        .then(resp => {
+            if (resp.rows.length > 0)
+                res.send({ message: `Row for ID ${req.params.recordId} found`, data: resp.rows });
+            else
+                res.status(404).send({ message: `No row exists for ID ${req.params.recordId}` });
+        })
+        .catch(err => res.status(500).send(err));
 });
 
+// /api/modify/:recordId
+// Update a record with new parameters
 router.put('/modify/:recordId', (req, res, next) => {
-    res.send(req.params);
+    if (!req.body.value1 || !req.body.value2 || !req.body.value3) return res.status(400).send({ message: 'All three values must be provided to complete this request' });
+    client
+        .query(`SELECT * FROM update_record(${req.params.recordId},'${req.body.value1}',${req.body.value2},${req.body.value3});`)
+        .then(resp => {
+            if (resp.rows.length > 0)
+                res.send({ message: `Row for ID ${req.params.recordId} updated`, data: resp.rows });
+            else
+                res.status(404).send({ message: `No row exists for ID ${req.params.recordId}` });
+        })
+        .catch(err => res.status(500).send(err));
 });
 
 router.delete('/remove/:recordId', (req, res, next) => {
-    res.send(req.params);
+    client
+        .query(`SELECT * FROM delete_record(${req.params.recordId});`)
+        .then(resp => {
+            if (resp.rows.length > 0)
+                res.send({ message: `Row for ID ${req.params.recordId} deleted`, data: resp.rows });
+            else
+                res.status(404).send({ message: `No row exists for ID ${req.params.recordId}` });
+        })
+        .catch(err => res.status(500).send(err));
 });
 
 module.exports = router;
